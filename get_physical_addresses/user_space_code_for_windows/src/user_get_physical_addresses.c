bb2aedeff96df64283eae89b8ed279cf427e89b4
PlusDino <tony900117471005@gmail.com>
2026-01-17 18:06:42 +0800

    Window : user space code Version_1

---
+++ get_physical_addresses/user_space_code_for_windows/src/user_get_physical_addresses.c
@@ -0,0 +1,21 @@
+#include "../include/user_get_physical_addresses.h"
+#include <stdlib.h>
+
+unsigned long user_get_physical_addresses(void* virtual_address){
+    unsigned long res;
+    res = 0x0000FFFF;// syscall(450, virtual_address, &res);
+    return res;
+}
+
+/*
+unsigned long* user_get_physical_addresses(unsigned long* virtual_address){
+    unsigned long* res = (unsigned long*)malloc(sizeof(unsigned long));
+    syscall(450, virtual_address, res);
+    return res;
+}
+
+int a = 123;
+unsigned long* physical_address = user_get_physical_addresses(&a);
+printf("the physical address of global variable a is %lx\n", *physical_address);
+free(physical_address;)
+*/
