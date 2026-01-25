2cfcc33a86c3b666ae765fd67bbcb4e6c8aa3b8f
PlusRon <tony900117471005@gmail.com>
2026-01-25 23:00:23 +0800

    SUCCESS : The picture of compile, execute, dmesg_info are in the 'hello_linux_test/pic/'.

--- hello_linux_test/kernel_space_code/hello_linux_test.c
+++ hello_linux_test/kernel_space_code/hello_linux_test.c
@@ -1,7 +1,7 @@
 #include <linux/kernel.h>
 #include <linux/syscalls.h>
 
-SYSCALL_DEFINE0(hello){
-    printk("Hello Linux Test!\n");
-    return 0;
+SYSCALL_DEFINE0(hello_linux_test){
+	printk("Hello Linux Test!\n");
+	return 0;
 }
